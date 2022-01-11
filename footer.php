28dfa523eb2998ed145ea51e3c85b017ede2401c
Mayowa Emmanuel <odudareemmanuel@gmail.com>
2022-01-11 13:34:46 +0100

    update name

--- footer.php
+++ footer.php
@@ -29,7 +29,7 @@
 				</div>
 			</section>
 			<section class="end-footer py-4">
-				<div class="container">
+				<div class="container mb-5 py-2">
 					<div class="row d-flex flex-column flex-md-row justify-content-between">
 						
 						<div class="col p-2">
@@ -52,12 +52,26 @@
 							</div>
 						</div>
 					</div>
-					<div class="container my-5">
-						<p class="text-center"> Copyright &copy <?php echo date('Y') ?> Doorsandhandles. All rights reserved.</p>
+					<div class="container my-5 pb-5">
+						<p class="text-center"> Copyright &copy <?php echo date('Y') ?> Doorsandhandle. All rights reserved.</p>
 					</div>
+					
 				</div>
 			</section>
 		</div><!-- .site-info -->
+		<div class="mobile-bottom-bar bg-white shadow-lg py-3 px-2 d-flex d-md-none justify-content-between position-fixed bottom-0 start-0 end-0" style="z-index:99">
+		<a href="<?php echo esc_url( home_url( '/' ) ).'/my-account'; ?>" rel="home">
+		<span class="text-primary"><i class="fa fa-user fa-2x"></i></span>            
+        </a>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+		<span class="text-primary"><i class="fa fa-home fa-2x"></i></span>            
+        </a>
+		<a href="<?php echo esc_url( home_url( '/' ) ).'/cart'; ?>" rel="home">
+		<span class="text-primary"><i class="fa fa-shopping-cart fa-2x"></i></span>            
+        </a>
+			
+
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- header.php
+++ header.php
@@ -100,7 +100,7 @@
 						<svg xmlns="http://www.w3.org/2000/svg" class="text-primary" style="height:1rem;width:1rem" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
 						</svg>
-						<sup class="bg-primary text-white px-1   rounded-circle" style="font-size: xx-small"><?php echo WC()->cart->get_cart_contents_count(); ?> </sup>
+						<!-- <sup class="bg-primary text-white px-1   rounded-circle" style="font-size: xx-small"><?php echo WC()->cart->get_cart_contents_count(); ?> </sup> -->
 						</a>
 					</div>
 						
@@ -129,7 +129,7 @@
 					<svg xmlns="http://www.w3.org/2000/svg" class="text-primary" style="height:1.5rem;width:1.5rem" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
 					</svg>
-					<sup class="bg-primary text-white px-1   rounded-circle" style="font-size: xx-small"><?php echo WC()->cart->get_cart_contents_count(); ?> </sup>
+					<!-- <sup class="bg-primary text-white px-1   rounded-circle" style="font-size: xx-small"><?php echo WC()->cart->get_cart_contents_count(); ?> </sup> -->
 					</a>
 					<!-- Toggle -->
 					<button class="navbar-toggler text-primary border-none" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
